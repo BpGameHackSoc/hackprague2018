564e23372ed9d62bf03bbe8a3d8a899cff92241c
Gergely Nemeth <neged.ng@gmail.com>
2018-06-24 07:29:38 +0200

    scripting getdata

--- src/getdata.py
+++ src/getdata.py
@@ -3,24 +3,27 @@
 import cv2
 import numpy as np
 
-frame_num = 30
-frame_count = 0
-camera = cv2.VideoCapture(0)
-while True:
-    frame_count += 1
-    return_value,image = camera.read()
-    flip = cv2.flip(image, 1)
-    gray = cv2.cvtColor(flip,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('image',gray)
-    if(frame_count>frame_num):
-        frame_count = 0
-        resized = cv2.resize(gray, (48,48))
-        resized = np.array(resized)
-        print(resized)
-        print(resized.shape)
-        # give gray to the NN
-    if cv2.waitKey(1)& 0xFF == ord('s'):
-        break
-camera.release()
-cv2.destroyAllWindows()
+def get_video_frames(out, frame_num=30, out_shape=(48,48)):
+    """Print the camera output into the out object"""
+    frame_count = 0
+    camera = cv2.VideoCapture(0)
+    while True:
+        frame_count += 1
+        return_value,image = camera.read()
+        flip = cv2.flip(image, 1)
+        gray = cv2.cvtColor(flip,cv2.COLOR_BGR2GRAY)
+        cv2.imshow('image',gray)
+        if(frame_count>frame_num):
+            frame_count = 0
+            resized = cv2.resize(gray, (48,48))
+            resized = np.array(resized)
+            out = resized
+            # give gray to the NN
+        if cv2.waitKey(1)& 0xFF == ord('q'):
+            break
+    camera.release()
+    cv2.destroyAllWindows()
 
+if __name__ == "__main__":
+    out = np.array(np.zeros((48,48)))
+    get_video_frames(out)
